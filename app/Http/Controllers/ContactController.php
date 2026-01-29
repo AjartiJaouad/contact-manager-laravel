18abf2181a200740a3e29d012d36cdf1e1dbff0b
ajartijaouad <ajartijaouad14@gmail.com>
2026-01-29 12:03:28 +0100

    feat: add store method with validation and saving contacts

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -13,9 +13,24 @@ class ContactController extends Controller
         return view('contacts.index', compact('contacts'));
     }
 
-        public function create(){
-            $groups =Group::all();
-            return view('contacts.create',compact('groups'));
-        }
+    public function create()
+    {
+        $groups = Group::all();
 
+        return view('contacts.create', compact('groups'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'nullable|email',
+            'phone' => 'nullable|string',
+            'group_id' => 'required|exists:groups,id',
+
+        ]);
+     
+    }
 }
